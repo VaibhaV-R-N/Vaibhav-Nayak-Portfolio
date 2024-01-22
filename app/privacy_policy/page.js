58a61a23962708bec82f3d3a6a050b1ca9d1c662
VaibhaV-R-N <vaibhavnayak2120@gmail.com>
2024-01-22 11:09:53 +0530

    privacy policy

---
+++ app/privacy_policy/page.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div className='w-full h-auto p-3 flex flex-col items-center justify-center gap-10'>
+        <h3 className='text-yellow-400 text-2xl'>Privacy Policy for vaibhav-nayak-portfolio</h3>
+        <p className='text-white text-xl'>This Privacy Policy describes how Vaibhav Nayak ("we," "us," or "our") collects, uses, and shares personal information when you visit and use the vaibhav-nayak-portfolio website (the "Website").</p>
+        <h3 className='text-yellow-400 text-2xl'>Information We Collect</h3>
+        <h4>Contact Information</h4>
+        <p className='text-white text-xl'>When you use the contact form on our website, we collect your name and email address</p>
+        <h4>Gmail API Information</h4>
+        <p className='text-white text-xl'>In order to send emails through the contact form, we use the Gmail API. This may involve collecting and processing your Gmail address and other necessary data for email communication.</p>
+        <h3 className='text-yellow-400 text-2xl'>How We Use Your Information</h3>
+        <p className='text-white text-xl'>We use the information you provide to respond to your inquiries and send emails through the Gmail API.</p>
+        <h3 className='text-yellow-400 text-2xl'>Information Sharing</h3>
+        <p className='text-white text-xl'>We do not sell or disclose your personal information to third parties, except as required by law or with your explicit consent.The Gmail API processes your data in accordance with Google's privacy policy.</p>
+        <h3 className='text-yellow-400 text-2xl'>Cookies and Analytics</h3>
+        <p className='text-white text-xl'>We may use cookies and similar technologies to analyze website traffic and enhance your user experience. You can control cookie settings in your browser.</p>
+        <h3 className='text-yellow-400 text-2xl'>Security</h3>
+        <p className='text-white text-xl'>We take reasonable measures to protect your information, but no online transmission or storage is 100% secure. Please use caution when submitting personal information online.</p>
+        <h3 className='text-yellow-400 text-2xl'>Your Choices</h3>
+        <p className='text-white text-xl'>You can contact us at vaibhavnayak243@gmail.com to update or delete your personal information.</p>
+        <h3 className='text-yellow-400 text-2xl'>Changes to This Privacy Policy</h3>
+        <p className='text-white text-xl'>We may update this Privacy Policy to reflect changes in our practices or for other operational, legal, or regulatory reasons.</p>
+        <h3 className='text-yellow-400 text-2xl'>Contact Us</h3>
+        <p className='text-white text-xl'>If you have questions about this Privacy Policy, please contact us at vaibhavnayak243@gmail.com .</p>
+        <p className='text-white text-xl'>This Privacy Policy was last updated on 22/01/24.</p>
+    </div>
+  )
+}
