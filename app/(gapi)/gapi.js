b5fa09df25a6547f854a9d149b2acadff800829d
VaibhaV-R-N <vaibhavnayak2120@gmail.com>
2024-01-22 10:29:12 +0530

    Update gapi.js

--- app/(gapi)/gapi.js
+++ app/(gapi)/gapi.js
@@ -3,7 +3,7 @@ import { google } from "googleapis";
 export const oAuthClient = new google.auth.OAuth2(
     process.env.GOOGLE_ID,
     process.env.GOOGLE_SECRET,
-    "http://localhost:3000/api/mailtoken"
+    "https://vaibhav-nayak-portfolio.vercel.app/api/mailtoken"
 )
 
 export const scopes=["https://www.googleapis.com/auth/gmail.send"]
