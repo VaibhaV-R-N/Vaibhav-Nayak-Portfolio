e9b691f93fbdaefa028b93413efe7772c109a32f
VaibhaV-R-N <vaibhavnayak2120@gmail.com>
2024-01-22 11:37:05 +0530

    terms

--- app/privacy_policy/page.js
+++ app/privacy_policy/page.js
@@ -4,27 +4,27 @@ export default function Page() {
   return (
     <div className='relative z-10 mt-4 w-full h-auto p-3 flex flex-col items-center justify-center gap-10'>
         <h3 className='text-yellow-400 text-2xl'>Privacy Policy for vaibhav-nayak-portfolio</h3>
-        <p className='text-white text-xl'>This Privacy Policy describes how Vaibhav Nayak (&quote;we,&quote; &quote;us,&quote; or &quote;our&quote;) collects, uses, and shares personal information when you visit and use the vaibhav-nayak-portfolio website (the &quote;Website&quote;).</p>
+        <p className='text-white text-md'>This Privacy Policy describes how Vaibhav Nayak (&quote;we,&quote; &quote;us,&quote; or &quote;our&quote;) collects, uses, and shares personal information when you visit and use the vaibhav-nayak-portfolio website (the &quote;Website&quote;).</p>
         <h3 className='text-yellow-400 text-2xl'>Information We Collect</h3>
-        <h4>Contact Information</h4>
-        <p className='text-white text-xl'>When you use the contact form on our website, we collect your name and email address</p>
-        <h4>Gmail API Information</h4>
-        <p className='text-white text-xl'>In order to send emails through the contact form, we use the Gmail API. This may involve collecting and processing your Gmail address and other necessary data for email communication.</p>
+        <h4 className='text-white text-xl'>Contact Information</h4>
+        <p className='text-white text-md'>When you use the contact form on our website, we collect your name and email address</p>
+        <h4 className='text-white text-xl'>Gmail API Information</h4>
+        <p className='text-white text-md'>In order to send emails through the contact form, we use the Gmail API. This may involve collecting and processing your Gmail address and other necessary data for email communication.</p>
         <h3 className='text-yellow-400 text-2xl'>How We Use Your Information</h3>
-        <p className='text-white text-xl'>We use the information you provide to respond to your inquiries and send emails through the Gmail API.</p>
+        <p className='text-white text-md'>We use the information you provide to respond to your inquiries and send emails through the Gmail API.</p>
         <h3 className='text-yellow-400 text-2xl'>Information Sharing</h3>
-        <p className='text-white text-xl'>We do not sell or disclose your personal information to third parties, except as required by law or with your explicit consent.The Gmail API processes your data in accordance with Google&apos;s privacy policy.</p>
+        <p className='text-white text-md'>We do not sell or disclose your personal information to third parties, except as required by law or with your explicit consent.The Gmail API processes your data in accordance with Google&apos;s privacy policy.</p>
         <h3 className='text-yellow-400 text-2xl'>Cookies and Analytics</h3>
-        <p className='text-white text-xl'>We may use cookies and similar technologies to analyze website traffic and enhance your user experience. You can control cookie settings in your browser.</p>
+        <p className='text-white text-md'>We may use cookies and similar technologies to analyze website traffic and enhance your user experience. You can control cookie settings in your browser.</p>
         <h3 className='text-yellow-400 text-2xl'>Security</h3>
-        <p className='text-white text-xl'>We take reasonable measures to protect your information, but no online transmission or storage is 100% secure. Please use caution when submitting personal information online.</p>
+        <p className='text-white text-md'>We take reasonable measures to protect your information, but no online transmission or storage is 100% secure. Please use caution when submitting personal information online.</p>
         <h3 className='text-yellow-400 text-2xl'>Your Choices</h3>
-        <p className='text-white text-xl'>You can contact us at vaibhavnayak243@gmail.com to update or delete your personal information.</p>
+        <p className='text-white text-md'>You can contact us at vaibhavnayak243@gmail.com to update or delete your personal information.</p>
         <h3 className='text-yellow-400 text-2xl'>Changes to This Privacy Policy</h3>
-        <p className='text-white text-xl'>We may update this Privacy Policy to reflect changes in our practices or for other operational, legal, or regulatory reasons.</p>
+        <p className='text-white text-md'>We may update this Privacy Policy to reflect changes in our practices or for other operational, legal, or regulatory reasons.</p>
         <h3 className='text-yellow-400 text-2xl'>Contact Us</h3>
-        <p className='text-white text-xl'>If you have questions about this Privacy Policy, please contact us at vaibhavnayak243@gmail.com .</p>
-        <p className='text-white text-xl'>This Privacy Policy was last updated on 22/01/24.</p>
+        <p className='text-white text-md'>If you have questions about this Privacy Policy, please contact us at vaibhavnayak243@gmail.com .</p>
+        <p className='text-white text-md'>This Privacy Policy was last updated on 22/01/24.</p>
     </div>
   )
 }
---
+++ app/terms/page.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div className='relative z-10 w-full h-auto p-3 mt-5 flex flex-col items-center jsutify-center gap-10'>
+        <h3 className='text-yellow-400 text-2xl'>Terms of Service for vaibhav-nayak-portfolio</h3>
+        <p className='text-white text-md'>These Terms of Service ("Terms") govern your use of the vaibhav-nayak-portfolio website (the "Website") operated by Vaibhav Nayak ("we," "us," or "our").By accessing or using the Website, you agree to be bound by these Terms. If you do not agree with any part of these Terms, please do not use the Website.</p>
+        <h3 className='text-yellow-400 text-2xl'>Use of the Website</h3>
+        
+        
+        <h4 className='text-white text-xl'>Content</h4>
+        <p className='text-white text-md'>The content on the Website, including text, images, and other materials, is provided for informational purposes only. You may not reproduce, distribute, or otherwise use any content from the Website without our prior written consent.</p>
+        
+        
+        <h4 className='text-white text-xl'>User Conduct</h4>
+        <p className='text-white text-md'>When using the Website, you agree not to engage in any unlawful or prohibited activities, including but not limited to the following:Violating any applicable laws or regulations.Interfering with the proper functioning of the Website.Attempting to gain unauthorized access to any part of the Website.</p>
+        
+        <h3 className='text-yellow-400 text-2xl'>Gmail API Usage</h3>
+        <h4 className='text-white text-xl'>Email Communication</h4>
+        <p className='text-white text-md'>If you use the contact form on our Website, you acknowledge and agree that we may use the Gmail API to send emails. Your use of the contact form constitutes your consent to the processing of your Gmail address and other necessary data for email communication.</p>
+        <h4 className='text-white text-xl'>Limitation of Liability</h4>
+        <p className='text-white text-md'> We are not responsible for any errors, omissions, or interruptions in the Gmail API service, and we disclaim any liability for damages resulting from its use.</p>
+        
+        <h3 className='text-yellow-400 text-2xl'>Privacy</h3>
+        
+        
+        <h4 className='text-white text-xl'>Privacy Policy</h4>
+        <p className='text-white text-md'>Your use of the Website is also governed by our Privacy Policy, which can be found <a href='https://vaibhav-nayak-portfolio.vercel.app/privacy_policy'>here</a>. By using the Website, you consent to the practices described in the Privacy Policy.</p>
+            
+        <h3 className='text-yellow-400 text-2xl'>Changes to Terms</h3>
+        <h4 className='text-white text-xl'>Modification</h4>
+        <p className='text-white text-md'>We reserve the right to modify or replace these Terms at any time. You will be notified of any material changes. Continued use of the Website after such changes constitutes your acceptance of the new Terms.</p>
+
+        <h3 className='text-yellow-400 text-2xl'>Contact Us</h3>
+        <h4 className='text-white text-xl'>Contact Information</h4>
+        <p className='text-white text-md'> If you have any questions about these Terms, please contact us at vaibhavnayak243@gmail.com.</p>
+
+    </div>
+  )
+}
